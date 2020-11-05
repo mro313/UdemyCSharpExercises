b44083b25ce1976e1e907e5e8a26366cde921b3d
mro313 <36976869+mro313@users.noreply.github.com>
2020-11-05 14:53:04 -0500

    Using CSharp_Section5ControlFlow_Exercises1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Section5ControlFlow_Exercises1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number, between 1-10");
+            
+            string userNumberString = Console.ReadLine();  // should it be "string" or "var"
+
+            int userNumber = Convert.ToInt32(userNumberString);
+
+            if (userNumber >= 1 && userNumber <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
