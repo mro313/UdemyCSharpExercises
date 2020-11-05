e6260390b5b69e372a37a6dcdb2e399188c0dfd1
mro313 <36976869+mro313@users.noreply.github.com>
2020-11-05 16:30:11 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,15 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+    // 1- Write a program and ask the user to enter a number. The number should be between 1 to 10. 
+    // If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid". 
+    // (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+
 namespace CSharp_Section5ControlFlow_Exercises1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Please enter a number, between 1-10");
             
-            string userNumberString = Console.ReadLine();  // should it be "string" or "var"
+            string userNumberString = Console.ReadLine(); 
 
             int userNumber = Convert.ToInt32(userNumberString);
 
